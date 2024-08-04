5f714ec57828db8f447aaa96748a988ef66b0032
Pranshu kumar <123887342+Pranshu936@users.noreply.github.com>
2024-08-04 08:16:12 +0530

    Update QR_Generator.py

--- QR_Generator.py
+++ QR_Generator.py
@@ -12,8 +12,8 @@ def gen_qr():
     qr_xbm = qr.xbm(scale=5)
     # Create a BitmapImage from the XBM data
     img = BitmapImage(data=qr_xbm, foreground='black', background='white')
-    # Update the label l4 to display the generated QR code
-    l4.config(image=img)
+    # Update the label l3 to display the generated QR code
+    l3.config(image=img)
 
 # Function to save the generated QR code as a PNG file
 def save():
@@ -43,11 +43,8 @@ f3.pack()
 # Create and place the labels and entry widget
 l1 = Label(f1, text='QR Code Generator', font=('SugarFont Bold', 30, 'bold'))
 l1.grid(row=0, column=0, columnspan=3, padx=5, pady=10)  # Main title
-l2 = Label(f1, text='By Harshit', font=('Arial Rounded MT Bold', 15))
-l2.grid(row=0, column=4, sticky='w', pady=10)  # Subtitle
-
-l3 = Label(f2, font=('', 10), text='Enter the content')
-l3.grid(row=0, column=0, padx=5)  # Label for entry widget
+l2 = Label(f2, font=('', 10), text='Enter the content')
+l2.grid(row=0, column=0, padx=5)  # Label for entry widget
 
 e1 = Entry(f2, textvariable=con, width=40)
 e1.grid(row=0, column=1, padx=5)  # Entry widget for user input
@@ -60,8 +57,8 @@ b2 = Button(f2, text='Save', command=save)
 b2.grid(row=1, column=1, pady=10)  # Button to save QR code
 
 # Label to display the generated QR code
-l4 = Label(f3, pady=5)
-l4.pack()
+l3 = Label(f3, pady=5)
+l3.pack()
 
 # Start the Tkinter event loop to keep the window open and responsive
 wind.mainloop()
